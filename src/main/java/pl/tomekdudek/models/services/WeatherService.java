cf8489753e87da14836afe56380368a024d7a8db
Tomek Dudek <ttomekk0@gmail.com>
2017-10-10 16:30:59 +0200

    added parseJsonData method

--- src/main/java/pl/tomekdudek/models/services/WeatherService.java
+++ src/main/java/pl/tomekdudek/models/services/WeatherService.java
@@ -1,6 +1,7 @@
 package pl.tomekdudek.models.services;
 
 
+import org.json.JSONObject;
 import pl.tomekdudek.models.Configuration;
 import pl.tomekdudek.models.Utils;
 
@@ -17,14 +18,42 @@ public class WeatherService {
     private String apiUrl;
     private String apiId;
 
+    private double temperature;
+    private int pressure;
+    private int humidity;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
     public void makeCall(String city) {
         Configuration configuration = new Configuration();
         apiUrl = configuration.getAPIURL();
         apiId = configuration.getAPIID();
 
         apiUrl = apiUrl + city + "&appid=" + apiId;
-        System.out.println(Utils.connectAndResponse(apiUrl));
+        parseJsonData((Utils.connectAndResponse(apiUrl)));
 
     }
+
+    public void parseJsonData(String data) {
+        JSONObject object = new JSONObject(data);
+        JSONObject mainObject = object.getJSONObject("main");
+        temperature = mainObject.getDouble("temp");
+        pressure = mainObject.getInt("pressure");
+        humidity = mainObject.getInt("humidity");
+
+    }
+
+  
+
 
 }
